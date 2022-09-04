b3bf538938318af7381f4ff742de6d3b27d4e13c
Fernando-MGS <3031970130108@ingenieria.usac.edu.gt>
2022-09-03 23:54:38 -0600

    Inicio de frontend. Se agregaron las vistas de las graficas

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import DataUsage from './components/usages';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+
+
+  return (
+    <div className="App">
+
+      <DataUsage />
+    </div>
+  );
+}
+
+export default App;
